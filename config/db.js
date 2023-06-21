9bbdf577114135fcfc0c43925710a8956b2f92e9
shehzadfalcon <shehzad.falcon@gmail.com>
2023-06-22 02:00:49 +0500

    first commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { createClient } = require("@supabase/supabase-js");
-const supabaseUrl = "https://ovwtvxkebmazfjnufzls.supabase.co";
+const supabaseUrl =process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
